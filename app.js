b1397767632e2f5dcaa1a5db5e55ffa00f231bd2
Musfira waseem <kwasim7844>
2024-02-27 03:30:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var quote = "Failure is an option here. If things are not failing, you are not innovating enough.";
+var author = "Elon Musk";
+console.log("".concat(author, " once said, \"").concat(quote));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let quote: string = "Failure is an option here. If things are not failing, you are not innovating enough.";
+let author: string= "Elon Musk"
+console.log(`${author} once said, "${quote}`);
